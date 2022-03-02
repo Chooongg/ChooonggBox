babb83eb02db24839020e154066c28120e0c641a
Chooongg <chooongg@outlook.com>
2022-03-02 10:57:57 +0800

    修复Toast不显示问题

--- utils/src/main/java/com/chooongg/ext/ToastExt.kt
+++ utils/src/main/java/com/chooongg/ext/ToastExt.kt
@@ -2,6 +2,7 @@ package com.chooongg.ext
 
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.GravityInt
 import androidx.annotation.StringRes
 import com.chooongg.manager.ActivityLifecycleManager
 
@@ -14,37 +15,70 @@ private var boxToast: Toast? = null
  * 展示 Toast
  */
 fun showToast(text: CharSequence?, duration: Int = Toast.LENGTH_SHORT) {
-    if (boxToast != null) {
-        boxToast!!.cancel()
-        boxToast!!.setText(text)
-        boxToast!!.duration = duration
-    } else {
-        boxToast = Toast.makeText(
-            ActivityLifecycleManager.activityTop as? Context ?: APPLICATION,
-            text,
-            duration
-        )
-    }
-    boxToast!!.show()
+    cancelToast()
+    boxToast = Toast.makeText(
+        ActivityLifecycleManager.activityTop as? Context ?: APPLICATION,
+        text,
+        duration
+    ).apply { show() }
 }
 
 /**
  * 展示 Toast
  */
 fun showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    if (boxToast != null) {
-        boxToast!!.cancel()
-        boxToast!!.setText(resId)
-        boxToast!!.duration = duration
-    } else {
-        boxToast = Toast.makeText(
-            ActivityLifecycleManager.activityTop as? Context ?: APPLICATION,
-            resId,
-            duration
-        )
-    }
-    boxToast!!.show()
+    cancelToast()
+    boxToast = Toast.makeText(
+        ActivityLifecycleManager.activityTop as? Context ?: APPLICATION,
+        resId,
+        duration
+    ).apply { show() }
 }
+
+/**
+ * 展示 Toast
+ */
+fun showToast(
+    text: CharSequence?,
+    @GravityInt gravity: Int,
+    xOffset: Int = 0,
+    yOffset: Int = 0,
+    duration: Int = Toast.LENGTH_SHORT
+) {
+    cancelToast()
+    boxToast = Toast.makeText(
+        ActivityLifecycleManager.activityTop as? Context ?: APPLICATION,
+        text,
+        duration
+    ).apply {
+        setGravity(gravity, xOffset, yOffset)
+        show()
+
+    }
+}
+
+/**
+ * 展示 Toast
+ */
+fun showToast(
+    @StringRes resId: Int,
+    @GravityInt gravity: Int,
+    xOffset: Int = 0,
+    yOffset: Int = 0,
+    duration: Int = Toast.LENGTH_SHORT
+) {
+    cancelToast()
+    boxToast = Toast.makeText(
+        ActivityLifecycleManager.activityTop as? Context ?: APPLICATION,
+        resId,
+        duration
+    ).apply {
+        setGravity(gravity, xOffset, yOffset)
+        show()
+
+    }
+}
+
 
 /**
  * 取消Toast
