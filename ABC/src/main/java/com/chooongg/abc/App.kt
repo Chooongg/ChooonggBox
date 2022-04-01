77bb92d6de58fe5903493158b41494e4755068fa
Chooongg <chooongg@outlook.com>
2022-04-01 17:13:48 +0800

    .

--- ABC/src/main/java/com/chooongg/abc/App.kt
+++ ABC/src/main/java/com/chooongg/abc/App.kt
@@ -1,6 +1,16 @@
 package com.chooongg.abc
 
 import android.app.Application
+import com.chooongg.statusLayout.StatusPage
+import com.chooongg.statusLayout.status.ProgressStatus
 
+@Suppress("unused")
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        StatusPage.configDefault {
+            defaultState = ProgressStatus::class
+        }
+    }
 }
