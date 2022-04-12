4e500f95a7aa13c47ebe98e0c8f36294eb05e92e
Chooongg <chooongg@outlook.com>
2022-04-12 18:34:54 +0800

    .

--- ABC/src/main/java/com/chooongg/abc/modules/TopAppbarActivity.kt
+++ ABC/src/main/java/com/chooongg/abc/modules/TopAppbarActivity.kt
@@ -3,8 +3,10 @@ package com.chooongg.abc.modules
 import android.os.Bundle
 import com.chooongg.abc.databinding.ActivityTopAppbarBinding
 import com.chooongg.core.activity.BoxBindingActivity
+import com.chooongg.core.annotation.TopAppBar
 
-class TopAppbarActivity:BoxBindingActivity<ActivityTopAppbarBinding>() {
+@TopAppBar(TopAppBar.TYPE_NONE)
+class TopAppbarActivity : BoxBindingActivity<ActivityTopAppbarBinding>() {
     override fun initConfig(savedInstanceState: Bundle?) {
     }
 
